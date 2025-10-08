6da9c5d65172ae78509b2d772a7c08b3566ac863
Gya09 <botcah.berkelas.09@gmail.com>
2025-10-08 10:59:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const browser = mp.browsers.new("package://speedo/index.html");
 
+// Update speedometer otomatis setiap frame
 mp.events.add('render', () => {
   const player = mp.players.local;
   const veh = player.vehicle;
 
   if (veh) {
-    const speed = veh.getSpeed() * 3.6; // convert m/s ke km/h
-    const rpm = veh.rpm * 8000;         // rpm range 0–8000
-    const fuel = 80;                    // contoh nilai statis
-    const health = veh.getBodyHealth() / 10; // 0–100
-    const nitro = 0;                    // jika belum punya sistem NOS
+    const speed = veh.getSpeed() * 3.6; // dari m/s ke km/h
+    const rpm = veh.rpm * 8000;         // dari 0-8000
+    const fuel = 100;                   // placeholder kalau belum ada sistem bensin
+    const health = veh.getBodyHealth() / 10; // dari 0–100
+    const nitro = 0;                    // kalau belum ada sistem NOS
     const gear = veh.gear;
 
     browser.execute(`updateSpeedometer({
-      speed: ${speed.toFixed(0)},
+      speed: ${speed.toFixed(1)},
       rpm: ${rpm.toFixed(0)},
       fuel: ${fuel.toFixed(0)},
       health: ${health.toFixed(0)},
